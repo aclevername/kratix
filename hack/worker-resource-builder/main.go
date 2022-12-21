0719acfa3a6dfc0b3b26ec7d82a067107d7ed2d6
Jake <jake@syntasso.io>
2022-12-21 16:27:00 +0000

    hack: worker-resource-builder puts workerClusterResources at end of output

--- hack/worker-resource-builder/main.go
+++ hack/worker-resource-builder/main.go
@@ -8,12 +8,15 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 
 	platformv1alpha1 "github.com/syntasso/kratix/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	yamlsig "sigs.k8s.io/yaml"
 )
+
+var l = log.New(os.Stderr, "", 0)
 
 func main() {
 	var resourcesPath string
@@ -67,13 +70,43 @@ func main() {
 	err = yaml.Unmarshal(promiseFile, &promise)
 	//If there's an error unmarshalling from the template, log to stderr so it doesn't silently go to the promise
 	if err != nil {
-		l := log.New(os.Stderr, "", 0)
 		l.Println(err.Error())
 		os.Exit(1)
 	}
 	promise.Spec.WorkerClusterResources = resources
 
-	//Write Promise (with workerClusterResources) to stdout
-	bytes, _ := yamlsig.Marshal(promise)
-	fmt.Println(string(bytes))
+	fmt.Println(marshallWithWorkerClusterResourcesAtEndOfSpec(promise))
+}
+
+func marshallWithWorkerClusterResourcesAtEndOfSpec(promise platformv1alpha1.Promise) string {
+	uniqueKey := "zWorkerClusterResources"
+
+	//replace workerClusterResources key with zWorkerClusterResources
+	promiseBytes := marshal(promise)
+	//this only matches with the top level workerClusterResources keys, won't reorder workerClusterResources inside a BBP
+	r := regexp.MustCompile("\n  workerClusterResources:\n")
+	yamlWithZKey := r.ReplaceAll(promiseBytes, []byte(fmt.Sprintf("\n  %s:\n", uniqueKey)))
+
+	//Unmarshal the yaml and remarshal it so zWorkerClusterResources comes after xaasCRD/RequestPipeline
+	i := make(map[string]interface{})
+	err := yaml.Unmarshal(yamlWithZKey, &i)
+	if err != nil {
+		l.Println(err.Error())
+		os.Exit(1)
+	}
+
+	//Marshal again and rename zWorkerClusterResources to workerClusterResources
+	promiseBytes = marshal(i)
+	r = regexp.MustCompile(fmt.Sprintf("\n  %s:\n", uniqueKey))
+	formattedYAML := r.ReplaceAll(promiseBytes, []byte("\n  workerClusterResources:\n"))
+	return string(formattedYAML)
+}
+
+func marshal(promise interface{}) []byte {
+	bytes, err := yamlsig.Marshal(promise)
+	if err != nil {
+		l.Println(err.Error())
+		os.Exit(1)
+	}
+	return bytes
 }
